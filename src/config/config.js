6fcb2b8f40db5aa4d7b622696da6eb5186f6f48e
Darshana Edirisinghe <mihiranlumiaa@outlook.com>
2024-05-13 23:28:04 +0530

    remove config.js file

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//     // dbURL: 'mongodb://localhost:27017/entertainment',
-//     dbURL: 'mongodb://mongodb:27017/entertainment',
-//     dbOptions: {
-//         directConnection: true,
-//         serverSelectionTimeoutMS: 2000,
-//         appName: 'mongosh+2.2.5'
-//     }
-// };
